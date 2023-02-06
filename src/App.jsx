1d6c2454e4f5c3e45230f450eb266d4a0ce5e444
eronmiranda <mirandaeronielle@gmail.com>
2023-02-06 00:38:31 -0700

    Add newsfeed and stats component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './App.css';
 import Header from './Header';
+import Newsfeed from './Newsfeed';
+import Stats from './Stats';
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -11,8 +13,15 @@ function App() {
 			<div className="app__header">
 				<Header />
 			</div>
-			<div className="app__body"></div>
+			<div className="app__body">
+				<div className="app__container">
+					<Newsfeed />
+					{/* stats */}
+					<Stats />
+				</div>
+			</div>
 			{/* Body */}
+			{/* 1:02:22 https://www.youtube.com/watch?v=TxggrQ0nHjY */}
 		</div>
 	);
 }
